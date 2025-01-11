546f01a9bc058e41d1cae17ba02226ba9ff087a5
deimos715 <deimos715@gmail.com>
2025-01-11 10:44:46 +0300

    update build

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,16 @@ import $ from 'jquery';
 */
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+/*
+ * Bootstrap 5 Accardion
+*/
+// import { Collapse } from 'bootstrap';
+
+/*
+ * Bootstrap 5 Alert
+*/
+// import { Alert } from 'bootstrap';
+
 
 /*
  * Custom
